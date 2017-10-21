8f4a04cc1759c93bf0fe8b5de6eb221890ba8228
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-10-21 10:19:05 -0500

    Add advanced session code

---
+++ README.md
@@ -0,0 +1,11 @@
+web-session-17
+==
+
+Webdev sessions for VandyHacks IV. This contains a real time, persistent todo-list app that syncs between multiple browsers.
+
+To run:
+
+    npm i
+    npm start
+
+Then visit `localhost:3000` to access the app.
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+require('express-ws')(app);
 
 const fs = require('fs');
 
@@ -14,13 +15,34 @@ if (fs.existsSync(itemsFile)) {
     items = fs.readFileSync(itemsFile);
 }
 
+let clients = new Set();
+
 app.get('/items/set/:items', (req, res) => {
     items = req.params.items;
-    fs.writeFile(itemsFile, items, () => {});
+    fs.writeFile(itemsFile, items, () => { });
+    for (let client of clients) {
+        try {
+            client.send(items);
+        } catch (ignored) {
+        }
+    }
     res.sendStatus(200);
 });
 app.get('/items', (req, res) => {
     res.send(items);
+});
+
+app.ws('/sock', (ws, req) => {
+    ws.on('message', msg => {
+        if (msg === 'join') {
+            console.log('joined');
+            clients.add(ws);
+        }
+    });
+    ws.on('close', () => {
+        console.log('closed');
+        clients.delete(ws);
+    });
 });
 
 app.listen(3000);
